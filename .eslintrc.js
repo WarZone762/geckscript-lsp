1bb31d33e62ed325268273852eae2a47b510d41b
WarZone762 <uzi1m1@gmail.com>
2022-07-05 20:36:05 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+/**@type {import('eslint').Linter.Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+	root: true,
+	parser: "@typescript-eslint/parser",
+	plugins: [
+		"@typescript-eslint",
+	],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+	],
+	rules: {
+		"semi": ["error", "always"],
+		"quotes": ["error", "double"],
+		"no-inner-declarations": "off",
+		"no-constant-condition": ["error", { checkLoops: false }],
+		// '@typescript-eslint/no-unused-vars': "off",
+		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/no-non-null-assertion": "off",
+		"@typescript-eslint/no-namespace": "off",
+		"@typescript-eslint/ban-types": "off",
+	}
+};
