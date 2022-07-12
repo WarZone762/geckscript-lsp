eec494311395d12a0baf850b37d093ca44e0b54f
WarZone762 <uzi1m1@gmail.com>
2022-07-12 09:08:58 +0300

    Started implementing new parser

--- src/test/test_geck_parsing.ts
+++ src/test/test_geck_parsing.ts
@@ -8,5 +8,5 @@ import * as Parser from "../geckscript/parser";
 const data = fs.readFileSync(path.join(__dirname, "./test_geck_parsing.gek")).toString();
 
 // Parser.Expression.PrettyPrint(Parser.GetAST(data));
-console.log(inspect(Parser.GetAST(data).getTokenAtPos({ column: 11, line: 12 }), false, null, true));
+console.log(inspect(Parser.GetAST(data), false, null, true));
 // console.log(inspect(Lexer.GetTokens(data), false, null, true));
